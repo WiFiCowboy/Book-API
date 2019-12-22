f36dd9508c725f3c84bb2fa7166a21a876f3cb6a
anotherchester <anotherchester@gmail.com>
2019-12-21 21:13:28 -0800

    really working app

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,18 +7,18 @@ export default class Item extends Component {
     const { item } = this.props;
 
     let { authors, title, description } = item.volumeInfo;
-    let { author } = authors[0];
-    //let {price} = item.saleInfo.retailPrice.amount;
-    //let {price} = listPrice.amount;
-    // let {image} = item.volumeInfo.imageLinks.thumbnail;
-
-    return (<li>
-      <h1>{author}</h1>
-      <h2>{title}</h2>
-      {/* <p>{price}</p>
-      <img src={image} /> */}
-      <p>{description}</p>
-    </li>)
+    let price = '';
+    if (item.saleInfo && item.saleInfo.retailPrice) {
+      price = item.saleInfo.retailPrice.amount;
+    }
+    return (
+      <li>
+        <h1>{authors}</h1>
+        <h2>{title}</h2>
+        <p>{price}</p>
+        <img src={item.volumeInfo.imageLinks.thumbnail} alt="book appearance" />
+        <p>{description}</p>
+      </li>)
 
   }
 }
